52e825bcf6e3a2c1c2e2ea365e53b05b465227b5
Carrik McNerlin <49008521+carrikm@users.noreply.github.com>
2021-05-06 11:45:04 -0400

    Update boardFunctions.py
    Put mine placement and board initialization into separate functions to increase readability.

--- boardFunctions.py
+++ boardFunctions.py
@@ -8,12 +8,7 @@
 import random
 
 # Mines are placed randomly and the tiles around them are incremented accordingly
-def createHiddenBoard(size, num_mines, bomb_char):
-    #default board of 0s
-    board = [[0 for row in range(size)] for column in range(size)]
-    
-
-    
+def placeMines(board, size, num_mines, bomb_char):
     for num in range(num_mines):
         #add bombs 
         new_space = False
@@ -24,10 +19,12 @@ def createHiddenBoard(size, num_mines, bomb_char):
                 new_space = True
             
         board[y][x] = bomb_char
+    
+        #******************************************************************************************
+        # Update count for tiles around the bombs
+        #******************************************************************************************
         
-        #update count for tiles around the bombs
-        
-        # if board[y][x] != bomb_char is in all of these because we don't want to change places that are mines.
+        # "if board[y][x] != bomb_char" is in all of these because we don't want to change places that are mines.
     
         # above and left
         if (x >= 1 and x <= size-1) and (y >= 1 and y <= size-1):
@@ -70,33 +67,50 @@ def createHiddenBoard(size, num_mines, bomb_char):
         if (x >= 1 and x <= size-2) and (y >= 0 and y <= size-2):
             if board[y+1][x+1] != bomb_char:
                 board[y+1][x+1] += 1
-                
+    # end for loop
+    
     return board
-#end createHiddenBoard()
+# end placeMines()
+
+
+
+# Create a 2-dimensional array of size n where each element is default_char
+def initializeBoard(default_char, n):
+    return [[default_char for row in range(n)] for col in range(n)]
+# end initializeBoard()
+
+
+# Make the actual board
+def createHiddenBoard(size, num_mines, bomb_char):
+    #default board of 0s
+    board = initializeBoard(0,size)
+    placeMines(board, size, num_mines, bomb_char)
+    return board
+# end createHiddenBoard()
 
 
 
 # Create the masked board that the user sees
 def createPlayBoard(size):
     #default board has an array of dashes(-)
-    board = [['-' for row in range(size)] for column in range(size)]
+    board = initializeBoard('-', size)
     return board
-#end createPlayBoard()
+# end createPlayBoard()
 
         
 # Replace the character at (y,x) on playBoard with the character from hiddenBoard
 def revealHidden(x, y, hiddenBoard, playBoard):
     playBoard[y][x] = hiddenBoard[y][x]
     return playBoard
-#end revealHidden()
+# end revealHidden()
 
 
 # Prints the board array that is provided in the form
 # [ . . . ]
 # [ . . . ]
 def printBoard(board, size):
+    print('\n')
     for col in range(size):
         print(board[col])
-    print('\n')
-#end printBoard()
+# end printBoard()
         
